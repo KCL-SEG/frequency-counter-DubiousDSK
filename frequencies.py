4ffa628e9141ab03665e27ba4fa43700f6660ddf
DubiousDSK <dilsadskaya@gmail.com>
2023-10-03 13:36:06 +0100

    Add the methods for the frequency counter

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,19 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    i = 0
+    while i < len(items):
+        if type(items[i]) == int:
+            items.remove(items[i])
+            items.append(str(items[i]))
+        ++i
+    
+    for x in items:
+        counter = 0
+        for y in items:
+            if x == y:
+                items.remove[y]
+                counter += 1
+        frequencies[x] = counter
+ 
     return frequencies
